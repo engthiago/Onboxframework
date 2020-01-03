f0131ce66a95f74c1e817165f3e1122bb9bb162b
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2020-01-03 22:40:56 +1030

    Added missing methos to IContainer Interface

--- Di/IContainer.cs
+++ Di/IContainer.cs
@@ -2,7 +2,9 @@
 {
     public interface IContainer
     {
+        void AddSingleton<TContract, TImplementation>();
         void AddSingleton<TContract, TImplementation>(TImplementation instance);
+        void AddSingleton<TImplementation>();
         void AddSingleton<TImplementation>(TImplementation instance);
         void AddTransient<TContract, TImplementation>();
         void AddTransient<TImplementation>();
