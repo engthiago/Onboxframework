7e3cd7dbf06a6cbafcf680f6c858dc146c2bba9e
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2020-01-10 07:58:50 +1030

    Added ColorPicker Control

---
+++ Mvc/ColorPicker.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Onbox.Mvc.V1
+{
+    public class ColorPicker : Control
+    {
+        public string Color
+        {
+            get { return (string)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register("Color", typeof(string), typeof(ColorPicker), new FrameworkPropertyMetadata("#ffffff", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault | FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public ColorPicker()
+        {
+            this.PreviewMouseDown += ColorPickerControl_PreviewMouseDown;
+        }
+
+        private void ColorPickerControl_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+            {
+                System.Windows.Forms.ColorDialog colorPicker = new System.Windows.Forms.ColorDialog
+                {
+                    AllowFullOpen = true,
+                    SolidColorOnly = true,
+                    FullOpen = true,
+                    Color = ColorTranslator.FromHtml(this.Color)
+                };
+
+                System.Windows.Forms.DialogResult result = colorPicker.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    if (colorPicker.Color != System.Drawing.Color.Empty)
+                    {
+                        this.Color = ColorTranslator.ToHtml(colorPicker.Color);
+                    }
+                }
+            }
+        }
+
+
+        static ColorPicker()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorPicker), new FrameworkPropertyMetadata(typeof(ColorPicker)));
+        }
+    }
+}
---
+++ Mvc/Converters/HtmlToBrushConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace Onbox.Mvc.V1.Converters
+{
+    public class HtmlToBrushConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var brush = (SolidColorBrush)(new BrushConverter().ConvertFrom(value.ToString()));
+                return brush;
+            }
+            catch
+            {
+                return new SolidColorBrush(Colors.Transparent);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var colorString = new BrushConverter().ConvertToString(value);
+                return colorString;
+            }
+            catch
+            {
+                return "ffffff";
+            }
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
