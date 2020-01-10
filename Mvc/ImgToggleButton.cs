42fb7b8f48256d00c67b713c3e4066ee47b75ade
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2020-01-10 13:30:27 +1030

    ImgToggle Image option list

--- Mvc/ImgToggleButton.cs
+++ Mvc/ImgToggleButton.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media.Imaging;
 
@@ -13,12 +14,41 @@ namespace Onbox.Mvc.V1
         }
 
         public static readonly DependencyProperty ImageProperty =
-            DependencyProperty.Register("Image", typeof(BitmapSource), typeof(ImgToggleButton), new PropertyMetadata(null));
+            DependencyProperty.Register("Image", typeof(BitmapSource), typeof(ImgToggleButton), 
+                new PropertyMetadata(new BitmapImage(new Uri(@"pack://application:,,,/Onbox.Mvc.V1;component/Resources/tglButton/Add0.png"))));
 
+        public ToggleImgButtonOptions Images
+        {
+            get { return (ToggleImgButtonOptions)GetValue(ImagesProperty); }
+            set { SetValue(ImagesProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImagesProperty =
+            DependencyProperty.Register("Images", typeof(ToggleImgButtonOptions), typeof(ImgToggleButton), new PropertyMetadata(ToggleImgButtonOptions.Add, OnImageOptionsChanged));
+
+        private static void OnImageOptionsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ImgToggleButton imgButton)
+            {
+                var name = e.NewValue.ToString();
+                imgButton.Image = new BitmapImage(new Uri($@"pack://application:,,,/Onbox.Mvc.V1;component/Resources/tglButton/{name}0.png"));
+            }
+        }
 
         static ImgToggleButton()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ImgToggleButton), new FrameworkPropertyMetadata(typeof(ImgToggleButton)));
         }
+    }
+
+    public enum ToggleImgButtonOptions
+    {
+        Add,
+        AddList,
+        Arrow,
+        Constraints,
+        PadLock,
+        Pin,
+        Undo
     }
 }
