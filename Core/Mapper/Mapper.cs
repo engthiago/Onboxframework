02de9af023f8b5970796a55448933dcc767822be
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2020-05-04 20:08:05 +0930

    Documented Mapper

--- Core/Mapper/Mapper.cs
+++ Core/Mapper/Mapper.cs
@@ -7,42 +7,75 @@ using System.Threading.Tasks;
 
 namespace Onbox.Core.V4.Mapper
 {
+    /// <summary>
+    /// Onbox Mapper can Clone objects and map properties and lists
+    /// </summary>
     public class Mapper
     {
+        private readonly MappingConfigurator mappingConfigurator;
+
+        /// <summary>
+        /// Creates a new mapper object
+        /// </summary>
+        /// <param name="mappingConfigurator">Optional configurator that allows for adding post map actions, you can pass null if not needed</param>
+        public Mapper(MappingConfigurator mappingConfigurator)
+        {
+            this.mappingConfigurator = mappingConfigurator;
+            if (this.mappingConfigurator == null)
+            {
+                this.mappingConfigurator = new MappingConfigurator();
+            }
+        }
+
+        /// <summary>
+        /// Clones an object
+        /// </summary>
+        /// <returns>The cloned object with all properties copied</returns>
         public TSorce Map<TSorce>(object source) where TSorce : new()
         {
             var target = new TSorce();
-            MappingConfigurator.Map(source, target);
+            mappingConfigurator.Map(source, target);
             return target;
         }
 
+        /// <summary>
+        /// Maps one object to another
+        /// </summary>
+        /// <param name="source">The source object</param>
+        /// <param name="target">The target objects that the properties will be copied to</param>
         public void Map<TSorce, TTarget>(TSorce source, TTarget target) where TSorce : new() where TTarget : new()
         {
-            MappingConfigurator.Map(source, target);
+            mappingConfigurator.Map(source, target);
         }
     }
 
-    public static class MappingConfigurator
+    /// <summary>
+    /// A Mapping configurator that will tell <see cref="Mapper"/> how to map objects
+    /// </summary>
+    public class MappingConfigurator
     {
-        static Dictionary<string, Delegate> keys = new Dictionary<string, Delegate>();
+        Dictionary<string, Delegate> keys = new Dictionary<string, Delegate>();
 
-        public static void AddMappingPostAction<TSource, TTaget>(Action<TSource, TTaget> action) where TSource : new() where TTaget : new()
+        /// <summary>
+        /// Adds a new an action that will be run after the map is complete
+        /// </summary>
+        public void AddMappingPostAction<TSource, TTaget>(Action<TSource, TTaget> action) where TSource : new() where TTaget : new()
         {
             var mapKey = typeof(TSource).FullName + typeof(TTaget).FullName;
             keys[mapKey] = action;
         }
 
-        private static Delegate GetMapFunction(Type source, Type target)
+        private Delegate GetMapFunction(Type source, Type target)
         {
             var mapKey = source.FullName + target.FullName;
-            if (keys.TryGetValue(mapKey, out Delegate result))
+            if (keys != null && keys.TryGetValue(mapKey, out Delegate result))
             {
                 return result;
             }
             return null;
         }
 
-        internal static object Map(object source, object target)
+        internal object Map(object source, object target)
         {
             if (source == null)
             {
@@ -90,7 +123,7 @@ namespace Onbox.Core.V4.Mapper
             return target;
         }
 
-        private static void CopyProperties(object source, object target, Type sourceType, Type targetType)
+        private void CopyProperties(object source, object target, Type sourceType, Type targetType)
         {
             var targetProps = targetType.GetProperties();
             var props = sourceType.GetProperties();
@@ -120,7 +153,7 @@ namespace Onbox.Core.V4.Mapper
             }
         }
 
-        internal static object Map(object source)
+        internal object Map(object source)
         {
             if (source == null)
             {
--- Tests/Core/MapperShould.cs
+++ Tests/Core/MapperShould.cs
@@ -34,7 +34,7 @@ namespace Core
                 }
             };
 
-            Mapper mapper = new Mapper();
+            Mapper mapper = new Mapper(null);
             var person2 = mapper.Map<Person>(person);
 
             person.FirstName = "Bruna";
@@ -46,7 +46,7 @@ namespace Core
         }
 
         [Test]
-        public void CloneExistingObject()
+        public void MapObjects()
         {
             var person = new Person
             {
@@ -62,7 +62,7 @@ namespace Core
             var person2 = new Person();
             person2.FirstName = "Bruna";
 
-            Mapper mapper = new Mapper();
+            Mapper mapper = new Mapper(null);
             mapper.Map(person, person2);
 
             Assert.That(person2.FirstName == person.FirstName);
@@ -70,31 +70,30 @@ namespace Core
             Assert.That(person2.Child.FirstName == person.Child.FirstName);
         }
 
-        //[Test]
-        //public void AddConfiguration()
-        //{
-        //    MappingConfigurator.AddMappingPostAction((Person p, Person p1) =>
-        //    {
-        //        p1.FirstName = "Daniel";
-        //    });
+        [Test]
+        public void AddConfigurator()
+        {
+            var mapconfig = new MappingConfigurator();
+            mapconfig.AddMappingPostAction((Person p, Person p1) =>
+            {
+                p1.FirstName = "Daniel";
+            });
 
-        //    var person = new Person
-        //    {
-        //        FirstName = "Thiago",
-        //        LastName = "Almeida",
-        //        Child = new Person
-        //        {
-        //            FirstName = "Jessica",
-        //            LastName = "Almeida"
-        //        }
-        //    };
+            Mapper mapper = new Mapper(mapconfig);
 
-
-        //    Mapper mapper = new Mapper();
-        //    var person2 = mapper.Map<Person>(person);
-
-        //    Assert.That(person2.FirstName == "Daniel");
-        //}
+            var person = new Person
+            {
+                FirstName = "Thiago",
+                LastName = "Almeida",
+                Child = new Person
+                {
+                    FirstName = "Jessica",
+                    LastName = "Almeida"
+                }
+            };
+            var person2 = mapper.Map<Person>(person);
+            Assert.That(person2.FirstName == "Daniel");
+        }
 
         [Test]
         public void MapLists()
@@ -104,7 +103,7 @@ namespace Core
 
             var list = new List<Person> { person, person2 };
 
-            Mapper mapper = new Mapper();
+            Mapper mapper = new Mapper(null);
             var list2 = mapper.Map<ObservableCollection<Person>>(list);
 
             var person3 = list2[0];
