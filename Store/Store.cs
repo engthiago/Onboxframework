7743fc2b26a6c459b9539edf2e0c820f73a0a647
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2020-05-05 17:23:24 +0930

    Store improvements

--- Store/Store.cs
+++ Store/Store.cs
@@ -3,19 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace Onbox.Store.V6
 {
-    public class StateHistory<T> where T : class, new() 
+    public class StateHistory<T> where T : class, new()
     {
         public DateTimeOffset UpdatedAt { get; set; }
         public T State { get; set; }
         public string Action { get; set; }
     }
 
-    public interface IStoreAction
+    public interface IStoreAction<T> where T : new()
     {
         string GetAction();
     }
@@ -44,29 +45,33 @@ namespace Onbox.Store.V6
         private T store = new T();
         private readonly IMapper mapper;
 
-        private readonly List<StateHistory<T>> storeHistory = new List<StateHistory<T>>();
+        private readonly List<StateHistory<T>> stateHistory = new List<StateHistory<T>>();
 
         public List<Delegate> maincallbacks = new List<Delegate>();
         public Dictionary<string, List<Delegate>> callbacks = new Dictionary<string, List<Delegate>>();
 
         public Store(IMapper mapper)
         {
-            this.storeHistory.Add(new StateHistory<T> { Action = null, State = null, UpdatedAt = DateTimeOffset.UtcNow });
+            this.stateHistory.Add(new StateHistory<T> { Action = null, State = null, UpdatedAt = DateTimeOffset.UtcNow });
             this.mapper = mapper;
         }
 
         public T State => this.store;
 
-        public StorageSubscription Subscribe<TState>(string action, Action<TState> callback)
+        public StorageSubscription Subscribe<TState>(IStoreAction<TState> action, Action<TState> callback) where TState : new()
         {
-            if (action == null)
+            var actionString = action.GetAction();
+
+            if (actionString == null)
             {
                 return new StorageSubscription(maincallbacks, callback);
             }
 
-            if (callbacks.ContainsKey(action))
+            EnsureActionType(action, actionString);
+
+            if (callbacks.ContainsKey(actionString))
             {
-                var list = callbacks[action];
+                var list = callbacks[actionString];
                 list.Add(callback);
                 return new StorageSubscription(list, callback);
             }
@@ -74,19 +79,63 @@ namespace Onbox.Store.V6
             {
                 var list = new List<Delegate>();
                 list.Add(callback);
-                callbacks.Add(action, list);
+                callbacks.Add(actionString, list);
                 return new StorageSubscription(list, callback);
             }
         }
 
-        public void SetState<TState>(TState state, string action) where TState : new()
+        private void EnsureActionType<TState>(IStoreAction<TState> action, string actionString) where TState : new()
         {
+            if (actionString != null)
+            {
+                var propertyType = GetPropertyType(typeof(T), actionString);
+                if (propertyType == null)
+                {
+                    throw new ArgumentException($"Action {action.GetType().FullName} has an invalid path: {actionString}");
+                }
+
+                var tpe = typeof(TState);
+                if (tpe != propertyType)
+                {
+                    throw new ArgumentException($"Action {action.GetType().FullName} has a property type mismatch, it should be {tpe.FullName} instead of {propertyType.FullName}");
+                }
+            }
+        }
+
+        public Type GetPropertyType(Type type, string path)
+        {
+            var currentType = type;
+            var pathArr = path?.Split('.');
+
+            foreach (var pathItem in pathArr)
+            {
+                if (currentType == null)
+                {
+                    return null;
+                }
+                var prop = currentType.GetProperty(pathItem);
+                if (prop == null)
+                {
+                    return null;
+                }
+
+                currentType = prop.PropertyType;
+            }
+
+            return currentType;
+        }
+
+        public void SetState<TState>(TState state, IStoreAction<TState> action) where TState : new()
+        {
+            var actionString = action?.GetAction();
+            EnsureActionType(action, actionString);
+
             var newStore = this.mapper.Map<T>(store);
-            var pathArr = action?.Split('.');
+            var pathArr = actionString?.Split('.');
 
             object currentObject = newStore;
 
-            if (pathArr!= null)
+            if (pathArr != null)
             {
                 foreach (var pathItem in pathArr)
                 {
@@ -107,22 +156,22 @@ namespace Onbox.Store.V6
 
             var stateEntry = new StateHistory<T>
             {
-                Action = action,
+                Action = actionString,
                 State = newStore,
                 UpdatedAt = DateTimeOffset.Now
             };
-            storeHistory.Add(stateEntry);
+            stateHistory.Add(stateEntry);
 
             store = newStore;
 
-            if (action != null && callbacks.ContainsKey(action))
+            if (actionString != null && callbacks.ContainsKey(actionString))
             {
-                foreach (var callback in callbacks[action])
+                foreach (var callback in callbacks[actionString])
                 {
                     callback.DynamicInvoke(currentObject);
                 }
             }
-            if (action == null)
+            if (actionString == null)
             {
                 foreach (var callback in maincallbacks)
                 {
--- Tests/Store/StoreShould.cs
+++ Tests/Store/StoreShould.cs
@@ -3,22 +3,66 @@ using Onbox.Core.V6;
 using Onbox.Mvc.V6;
 using Onbox.Store.V6;
 using System;
+using System.Reflection;
 
 namespace Tests.Store
 {
     [TestFixture]
     public class StoreShould
     {
+        public class Pet
+        {
+            public string Name { get; set; }
+        }
+
+        public class Cat : Pet
+        {
+        }
+
         public class Person : NotifyPropertyBase
         {
             public string FirstName { get; set; }
             public string LastName { get; set; }
+
+            public Pet Pet { get; set; }
 
             public Person Child { get; set; }
         }
 
         public StoreShould()
         {
+        }
+
+        public class PersonAction : IStoreAction<Person>
+        {
+            public string GetAction()
+            {
+                return null;
+            }
+        }
+
+        public class ChildAction : IStoreAction<Person>
+        {
+            public string GetAction()
+            {
+                return "Child";
+            }
+        }
+
+        public class PetAction : IStoreAction<Pet>
+        {
+            public string GetAction()
+            {
+                return "Pet";
+            }
+        }
+
+        public class ChildChildAction : IStoreAction<Person>
+        {
+            public string GetAction()
+            {
+                return "Child.Child";
+            }
         }
 
         [Test]
@@ -31,26 +75,33 @@ namespace Tests.Store
 
             var store = container.Resolve<Store<Person>>();
 
-            var subs = store.Subscribe<Person>("Child.Child", (person) =>
+            var subs = store.Subscribe(new ChildChildAction(), (person) =>
             {
-               Console.WriteLine("Updated person: Child child");
+                Console.WriteLine("Updated person: Child child");
             });
 
-            var subs2 = store.Subscribe<Person>("Child", (person) =>
+            var subs2 = store.Subscribe(new ChildAction(), (person) =>
+            {
+                Console.WriteLine("Updated person: Child");
+            });
+
+            var subs3 = store.Subscribe(new PetAction(), (pet) =>
             {
                 Console.WriteLine("Updated person: Child");
             });
 
 
-            store.SetState(new Person { FirstName = "Thiago", LastName = "Almeida" }, null);
-            store.SetState(new Person { FirstName = "Arnold", LastName = "Almeida" }, "Child");
-            store.SetState(new Person { FirstName = "Bruno", LastName = "Almeida" }, "Child.Child");
+            store.SetState(new Person { FirstName = "Thiago", LastName = "Almeida" }, new PersonAction());
+            store.SetState(new Person { FirstName = "Arnold", LastName = "Almeida" }, new ChildAction());
+            store.SetState(new Person { FirstName = "Bruno", LastName = "Almeida" }, new ChildChildAction());
+            store.SetState(new Cat { Name = "Caetano" }, new PetAction());
 
 
             Assert.That(store.State.Child.Child.FirstName == "Bruno");
 
             subs.Unsubscribe();
             subs2.Unsubscribe();
+            subs3.Unsubscribe();
         }
 
         private void Store_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
