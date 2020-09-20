beb9275c60898a6b3be38d5d51a86171c1564136
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2020-09-20 17:26:00 +0930

    Revit Async Extension method

---
+++ src/Revit/RevitExternalHandlerExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Onbox.Abstractions.V7;
+using System;
+
+namespace Onbox.Revit.V7
+{
+    public static class RevitAsyncExtensions
+    {
+        /// <summary>
+        /// Adds support for making async calls to Revit 
+        /// </summary>
+        public static void AddRevitAsync(this IContainer container, Action<RevitAsyncSettings> config)
+        {
+            if (config == null)
+            {
+                throw new ArgumentException("Adding Revit Async should have a valid configuration.");
+            }
+
+            var settings = new RevitAsyncSettings();
+            config.Invoke(settings);
+
+            if (string.IsNullOrWhiteSpace(settings.Name))
+            {
+                throw new ArgumentException("Adding Revit Async should have a valid handler name.");
+            }
+
+            container.AddSingleton(settings);
+            container.AddSingleton<IRevitEventHandler, RevitExternalHandler>();
+        }
+    }
+}
