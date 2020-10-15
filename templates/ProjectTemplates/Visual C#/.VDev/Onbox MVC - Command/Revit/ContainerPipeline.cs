c89880697dfe6749d046bd9c627ee201967e5e33
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2020-10-15 17:05:55 +1030

    Improvements to VS Templates - Added MVC Command as Project template

---
+++ templates/ProjectTemplates/Visual C#/.VDev/Onbox MVC - Command/Revit/ContainerPipeline.cs
@@ -0,0 +1,22 @@
+﻿using Onbox.Abstractions.VDev;
+using Onbox.Core.VDev;
+using Onbox.Mvc.Revit.VDev;
+using $safeprojectname$.Services;
+
+namespace $safeprojectname$.Revit
+{
+    public class ContainerPipeline : IContainerPipeline
+    {
+        public IContainer Pipe(IContainer container)
+        {
+            container.AddOnboxCore();
+            container.AddRevitMvc();
+
+            // Adds MessageBoxService to the container
+            container.AddSingleton<IMessageService, TaskMessageService>();
+
+            return container;
+        }
+    }
+
+}
