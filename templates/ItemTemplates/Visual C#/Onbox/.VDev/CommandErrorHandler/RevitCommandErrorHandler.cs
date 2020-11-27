02ffbdb89a02b3a23389c061db26b8a0c8610cf8
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2020-11-28 00:54:35 +1030

    Added project templates for RevitCommandErrorHandler and RevitCommandGuard

---
+++ templates/ItemTemplates/Visual C#/Onbox/.VDev/CommandErrorHandler/RevitCommandErrorHandler.cs
@@ -0,0 +1,16 @@
+﻿using Onbox.Revit.VDev.Commands;
+using Onbox.Revit.VDev.Commands.ErrorHandlers;
+using System;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : IRevitCommandErrorHandler
+    {
+        public bool Handle(ICommandInfo commandInfo, Exception exception)
+        {
+            // You can use this method to log errors, display error messages, swallow exceptions and so on.
+
+            return true;
+        }
+    }
+}
---
+++ templates/ItemTemplates/Visual C#/Onbox/.VDev/CommandGuard/CommandGuard1.cs
@@ -0,0 +1,14 @@
+﻿using Onbox.Revit.VDev.Commands;
+using Onbox.Revit.VDev.Commands.Guards;
+
+namespace CommandGuardSamples.Commands.Guards
+{
+    public class $safeitemname$ : IRevitCommandGuard
+    {
+        public bool CanExecute(ICommandInfo commandInfo)
+        {
+			// Return true to allow Command execution
+            return true;
+        }
+    }
+}
