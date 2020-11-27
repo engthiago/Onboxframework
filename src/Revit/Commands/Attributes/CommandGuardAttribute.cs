580432c6519c63a1e571da7f67a6a73bf90d1d23
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2020-11-27 17:44:43 +1030

    Added documentation to CommandGuardAttribute

--- src/Revit/Commands/Attributes/CommandGuardAttribute.cs
+++ src/Revit/Commands/Attributes/CommandGuardAttribute.cs
@@ -4,16 +4,28 @@ using System.Linq;
 
 namespace Onbox.Revit.VDev.Commands.Attributes
 {
+    /// <summary>
+    /// Adds a CommandGuard to to a Revit Command.
+    /// <br>The type MUST implement <see cref="IRevitCommandGuard"/> interface!</br>
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
     public class CommandGuardAttribute : Attribute
     {
         private readonly Type revitCommandGuardType;
 
+        /// <summary>
+        ///  Adds a CommandGuard to to a Revit Command.
+        /// </summary>
+        /// <param name="revitCommandGuardType">The type mus implement <see cref="IRevitCommandGuard"/> interface</param>
         public CommandGuardAttribute(Type revitCommandGuardType)
         {
             this.revitCommandGuardType = revitCommandGuardType;
         }
 
+        /// <summary>
+        /// Gets the CommandGuard Type
+        /// </summary>
+        /// <returns>The type of the Command Guard</returns>
         public Type GetCommandGuardType()
         {
             return this.revitCommandGuardType;
