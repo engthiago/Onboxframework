5ffc439da41ffbdd5d8fcd7d41edebda66d3f9f3
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2021-01-17 07:15:30 +1030

    Improved //summary for IContainerConsolePrint

--- src/Abstractions/IContainerConsolePrint.cs
+++ src/Abstractions/IContainerConsolePrint.cs
@@ -6,7 +6,7 @@
     public interface IContainerConsolePrint
     {
         /// <summary>
-        /// Enables console printing for important events on the container. E.g: When the container is instantiating an object.
+        /// Enables console writeline for important events on the container. E.g: When the container is instantiating an object.
         /// </summary>
         /// <param name="enabled">flag to enable or disable console priting.</param>
         void EnableConsolePrinting(bool enabled);
