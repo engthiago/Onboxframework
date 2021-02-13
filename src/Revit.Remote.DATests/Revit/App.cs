eff58b136a4ae47ea21a108a549245111dece744
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2021-02-14 09:44:16 +1030

    Improved test DA logic

--- src/Revit.Remote.DATests/Revit/App.cs
+++ src/Revit.Remote.DATests/Revit/App.cs
@@ -1,12 +1,15 @@
 ﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Revit.NUnit.Engine;
 
 namespace Onbox.Revit.Remote.DATests.Revit
 {
+    [Regeneration(RegenerationOption.Manual)]
+    [Transaction(TransactionMode.Manual)]
     public class App : RevitTestRunnerApp
     {
-        public override ExternalDBApplicationResult OnStartup(IRevitTestRunner tests, ControlledApplication application)
+        public override ExternalDBApplicationResult OnStartup(IRevitTestRunner tests, Application application)
         {
             tests.Run("Onbox.Revit.Remote.Tests.dll", "result.xml");
 
--- src/Revit.Remote.DATests/RevitTestRunnerApp.cs
+++ src/Revit.Remote.DATests/RevitTestRunnerApp.cs
@@ -3,32 +3,41 @@ using Autodesk.Revit.DB;
 using DesignAutomationFramework;
 using Onbox.Revit.NUnit;
 using Revit.NUnit.Engine;
+using System;
 
 namespace Onbox.Revit.Remote.DATests
 {
     public abstract class RevitTestRunnerApp : IExternalDBApplication
     {
-        private ControlledApplication application;
-
         public ExternalDBApplicationResult OnStartup(ControlledApplication application)
         {
-            this.application = application;
             DesignAutomationBridge.DesignAutomationReadyEvent += OnAppReady;
             return ExternalDBApplicationResult.Succeeded;
         }
 
         private void OnAppReady(object sender, DesignAutomationReadyEventArgs e)
         {
+            var doc = e.DesignAutomationData.RevitDoc;
+            var app = e.DesignAutomationData.RevitApp;
+
+            if (e.DesignAutomationData.RevitDoc == null)
+            {
+                doc = app.NewProjectDocument(UnitSystem.Metric);
+                if (doc == null)
+                {
+                    throw new InvalidOperationException("Could not create new document.");
+                }
+            }
+
             RevitRemoteContainer.Initialize(
-                e.DesignAutomationData.RevitDoc, e.DesignAutomationData.RevitApp, e.DesignAutomationData.FilePath);
+                doc, e.DesignAutomationData.RevitApp, e.DesignAutomationData.FilePath);
 
             var testRunner = new RevitTestRunner();
-            this.OnStartup(testRunner, application);
+            this.OnStartup(testRunner, e.DesignAutomationData.RevitApp);
         }
 
-        public abstract ExternalDBApplicationResult OnStartup(IRevitTestRunner tests, ControlledApplication application);
+        public abstract ExternalDBApplicationResult OnStartup(IRevitTestRunner tests, Application application);
 
         public abstract ExternalDBApplicationResult OnShutdown(ControlledApplication application);
-
     }
 }
